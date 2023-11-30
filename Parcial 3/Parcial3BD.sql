fd7dd6d826edcaab8245ccf949b663045c2ceb4d
MoiB03 <moises.betancourt@utp.ac.pa>
2023-11-29 22:05:30 -0500

    Agregando la BD

---
+++ Parcial 3/Parcial3BD.sql
@@ -0,0 +1,20 @@
+create database Parcial3
+
+use Parcial3
+
+drop table Datos
+
+create table Datos(
+
+	header nvarchar,
+	inicio nvarchar,
+	personajes_inicio nvarchar,
+	arcos nvarchar,
+	openings nvarchar,
+	formulario nvarchar,
+	footer nvarchar,
+
+)
+
+insert into Datos(header,inicio, personajes_inicio,arcos,openings,formulario,footer)
+		   values()
